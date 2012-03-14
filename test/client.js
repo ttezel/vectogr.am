a259880de02cf7628cb699ed4c4dcf917defbd64
Tolga Tezel <tolgatezel11@gmail.com>
2012-03-14 19:27:20 -0400

    client updates

--- test/client.js
+++ test/client.js
@@ -1,38 +1,55 @@
 var http = require('http')
-  , Server = require('../lib/server')
+  , querystring = require('querystring')
+  , Vectogram = require('../lib/vectogram');
 
 //
 //  Client
 //
-var Client = function () {
-  this.opts = {
-      host: '127.0.0.1'
-    , port: 8000
-  }
-}
+var Client = function (opts) {
+  this.opts = opts;
+};
 
-Client.prototype.makeRequest = function (apiKey, authHash, imgUrl) {
-  this.opts.path = '/' + apiKey + '/' + authHash + '/' + imgUrl;
+Client.prototype.makeRequest = function (imgUrl, pathonly) {
+  var params = { pathonly: pathonly };
 
-  var req =  http.get(this.opts, function (res) {
+  this.opts.headers = { 'connection': 'keep-alive' };
+  this.opts.method = 'GET';
+  this.opts.path = '/v1/' + imgUrl + '?' + querystring.stringify(params);
+
+  var req =  http.request(this.opts, function (res) {
     var reply = '';
+
+    console.log('statusCode:', res.statusCode);
+    
     res.on('data', function (chunk) {
       reply += chunk;
     });
     res.on('end', function () {
-      console.log('CLIENT: reply', reply)
+
+      var fd = require('path').resolve(__dirname, '../files/testpath.txt');
+      require('fs').writeFileSync(fd, reply);
+
     });
   });
-
   req.on('error', function (e) {
-    console.log('http client error:', e)
+    console.log('CLIENT error:', e);
   });
-}
+  req.end();
+};
 
 //
 //tests
 //
-var server = new Server()
-  , client = new Client();
 
-client.makeRequest('foo', 'bar', 'http://jquery.com/demo/thickbox/images/plant4.jpg');
+var opts = { port: 8000, host: '127.0.0.1' };
+
+var server = new Vectogram(opts)
+  , client = new Client(opts);
+
+//client.makeRequest('http://jquery.com/demo/thickbox/images/plant4.jpg', true);
+//client.makeRequest('http://jquery.com/demo/thickbox/images/plant4.jpg', false);
+//client.makeRequest('http://upload.wikimedia.org/wikipedia/commons/d/d5/Dds40-097_large.jpeg', true)
+//client.makeRequest('http://t0.gstatic.com/images?q=tbn:ANd9GcRrBs5epg4w79Ral9TeQ40u2CFQGipgz_T1VH6YwnbglT1A8un_bszOByKqCg', true);
+client.makeRequest('http://www.gly.uga.edu/railsback/11111misc/SizeofThings36Large.jpeg', true);
+//client.makeRequest('http://www.google.com/images/srpr/logo3w.png', true);
+//client.makeRequest(undefined, undefined)
